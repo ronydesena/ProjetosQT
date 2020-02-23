e01732f7586e74dbaed9935d8ae7fb3e87296091
Rony de Sena <rony_desena@yahoo.com>
2020-02-23 01:33:09 -0300

    estudo

---
+++ Aula_QT_05/mainwindow.cpp
@@ -0,0 +1,20 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(mudaTexto()));
+}
+
+void MainWindow::mudaTexto()
+{
+    ui->label->setText("Rony Sena");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
